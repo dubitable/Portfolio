f57a40a71b9229aca81cc3ffd36a521d6209bb8f
dubitable <pierrequereuil@gmail.com>
2024-05-21 08:00:46 -0700

    Header + Footer

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import { json, LoaderFunctionArgs, TypedResponse } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getUserInfo } from "~/.server/auth";
+import Footer from "~/components/Footer";
+import Header from "~/components/Header";
 import { getSession } from "~/sessions";
 
 type LoaderData = {
@@ -26,10 +28,14 @@ export const loader = async ({
 const Index = () => {
   const user = useLoaderData<typeof loader>();
   return (
-    <div className="h-screen bg-slate-700 flex justify-center items-center">
-      <h2 className="text-blue-200 font-extrabold text-5xl">
-        welcome {user.username ?? ""}
-      </h2>
+    <div className="h-screen bg-white flex flex-col justify-between align-items">
+      <Header />
+      <div className="flex justify-center">
+        <h2 className="text-black font-extrabold text-3xl">
+          welcome {user.username ?? ""}
+        </h2>
+      </div>
+      <Footer />
     </div>
   );
 };
